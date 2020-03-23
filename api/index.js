6862c02c1c29179e9888f6c5f336a05f3bb64e1b
中島　亮三 <aki323buri2@gmail.com>
2020-03-24 01:40:29 +0900

    Apollo Serverがproductionでもplaygroundをオンにする

--- api/index.js
+++ api/index.js
@@ -7,6 +7,10 @@ const typeDefs = require('./typeDefs');
 const server = new ApolloServer({
   resolvers, 
   typeDefs, 
+  // productionでfalseになるので
+  // https://spectrum.chat/zeit/general/apollo-server-get-query-missing~ae8d5ca5-2faf-4c8d-8097-c3f6e25cad56
+  introspection: true, 
+  playground: true, 
 });
 
 server.listen().then(({ url }) => {
